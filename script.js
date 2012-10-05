52ca7673fbcbd292ff4b467edc8d6ecf8f0ad87b
Andy Delcambre <adelcambre@gmail.com>
2012-10-05 17:16:03 +0200

    calculate altitude

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
+var altitude = undefined;
+
+client.config('general:navdata_demo', 'FALSE');
+
+client.on('navdata', function(data) {
+  if (data.rawMeasures) {
+    altitude = data.rawMeasures.altTempRaw;
+  }
+});
 
 client.takeoff();
 
-client
-  .after(5000, function() {
-        this.clockwise(0.5);
-          })
-  .after(3000, function() {
-    this.up(1);
-   })
-  .after(1000, function() {
-    this.animate('flipAhead');
-  })
-  .after(3000, function() {
-        this.stop();
-        this.land();
- });
