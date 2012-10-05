fa573618ade94ed5a1aca0fb04c7e237aa8f3c95
Andy Delcambre <adelcambre@gmail.com>
2012-10-05 12:29:26 +0200

    more keys

--- keys.js
+++ keys.js
@@ -1,20 +1,35 @@
 var arDrone = require('ar-drone');                                              
 var client  = arDrone.createClient();
-client.on('navdata', console.log);
+// client.on('navdata', console.log);
 
-var stdin = process.openStdin(); 
-require('tty').setRawMode(true);    
+var stdin = process.stdin;
+// require('tty').setRawMode(true);
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
+// var newData = null;
 
-stdin.on('keypress', function (chunk, key) {
-    process.stdout.write('Get Chunk: ' + chunk + '\n');
-    process.stdout.write('pressed ' + key.name + '\n'); 
-    if(key.name == 't')
+stdin.on('data', function (key) {
+    // newData = new Date().getTime();
+    process.stdout.write('pressed ' + key + '\n');
+    if(key == 't')
     {
       process.stdout.write('Taking off');
       client.takeoff();
     }
-    if(key.name == 'g')
+    if(key == 'g')
       client.land();
-
-    if (key && key.ctrl && key.name == 'c') process.exit();
+    if(key == 'j')
+      client.counterClockwise(0.5);
+    if(key == 'k')
+      client.clockwise(0.5);
+    if(key == 'x')
+      client.stop();
+    if (key && key == '\u0003') process.exit();
 });
+
+// setInterval(function() {
+//   if((new Date().getTime() - newData) > 10) {
+//     client.stop();
+//   }
+// }, 10);
