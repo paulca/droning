bee5b6a1094d73bbd5d5619f78c990ab85026868
Paul Campbell <paul@rslw.com>
2012-10-05 17:27:09 +0200

    change the threshold based on the volume of the input

--- drone-noise.coffee
+++ drone-noise.coffee
@@ -8,12 +8,43 @@ last_state = null
 setInterval ->
   fs.readFile file, 'utf8', (err,data) ->
     tier = parseInt(data)
-    switch tier
-    if parseInt(data) < -5
-      state = 'land'
-    else
+    if tier > -5
       state = 'up'
+    else
+      state = 'land'
+    if tier < -5
+      threshold = 0  
+    else if tier < -5
+      threshold = 800
+    else if tier < -4
+      threshold = 1000
+    else if tier < -3
+      threshold = 1200
+    else if tier < -2
+      threshold = 1400
+    else if tier < -1
+      threshold = 1600
+    else if tier < 0
+      threshold = 1800
+    else if tier < 1
+      threshold = 2000
+    else if tier < 2
+      threshold = 2200
+    else if tier < 3
+      threshold = 2400
+    else if tier < 4
+      threshold = 2600
+    else if tier < 5
+      threshold = 2800
+    else if tier > 6
+      threshold = 3000
+      state = 'flip'
+
     if state != last_state
-      console.log(state)
+      console.log("State: #{state}")
+    if threshold != last_threshold
+      console.log("Threshold: #{threshold}")
+
     last_state = state
+    last_threshold = threshold
 , 30
--- drone-noise.js
+++ drone-noise.js
@@ -14,17 +14,49 @@
 
   setInterval(function() {
     return fs.readFile(file, 'utf8', function(err, data) {
-      var tier;
+      var last_threshold, threshold, tier;
       tier = parseInt(data);
-      if (parseInt(data) < -5) {
-        state = 'land';
-      } else {
+      if (tier > -5) {
         state = 'up';
+      } else {
+        state = 'land';
+      }
+      if (tier < -5) {
+        threshold = 0;
+      } else if (tier < -5) {
+        threshold = 800;
+      } else if (tier < -4) {
+        threshold = 1000;
+      } else if (tier < -3) {
+        threshold = 1200;
+      } else if (tier < -2) {
+        threshold = 1400;
+      } else if (tier < -1) {
+        threshold = 1600;
+      } else if (tier < 0) {
+        threshold = 1800;
+      } else if (tier < 1) {
+        threshold = 2000;
+      } else if (tier < 2) {
+        threshold = 2200;
+      } else if (tier < 3) {
+        threshold = 2400;
+      } else if (tier < 4) {
+        threshold = 2600;
+      } else if (tier < 5) {
+        threshold = 2800;
+      } else if (tier > 6) {
+        threshold = 3000;
+        state = 'flip';
       }
       if (state !== last_state) {
-        console.log(state);
+        console.log("State: " + state);
       }
-      return last_state = state;
+      if (threshold !== last_threshold) {
+        console.log("Threshold: " + threshold);
+      }
+      last_state = state;
+      return last_threshold = threshold;
     });
   }, 30);
 
